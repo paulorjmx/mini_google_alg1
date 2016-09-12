71eb8764fbb55d978798a3f6a837c9ac41ecc6b0
Paulo Ricardo <pauloricardojm@hotmail.com.br>
2016-09-11 21:40:40 -0300

    Added an erase list function

--- inc/list.h
+++ inc/list.h
@@ -1,5 +1,9 @@
 #ifndef LIST_H
     #define LIST_H
 
-        typedef list LIST;
+        typedef struct list LIST;
+
+        LIST *create_list();
+        void add_node(LIST *begin, char *cod, char *nome, unsigned int relevancia, char *link, KEYWORDS *keywords);
+        void erase_list(LIST *begin);
 #endif
