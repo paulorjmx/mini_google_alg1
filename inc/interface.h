8d71236e8e874ef71cbd160fccd4e4d97e06fdd5
paulorjmx <pauloricardojm@hotmail.com.br>
2016-09-27 19:44:01 -0300

    Added list and keywords libraries

--- inc/interface.h
+++ inc/interface.h
@@ -1,10 +1,11 @@
 #ifndef INTERFACE_H
     #define INTERFACE_H
+        #include "list.h"
+        #include "keywords.h"
 
-    void menu(LIST *l);
-    void menu_inserir(LIST *l);
-    void menu_remover(LIST *l);
-    void menu_inserir_pchave(LIST *l);
-    void menu_atualizar_relevancia(LIST *l);
-
+        void menu(LIST *l);
+        void menu_inserir(LIST *l);
+        void menu_remover(LIST *l);
+        void menu_inserir_pchave(LIST *l);
+        void menu_atualizar_relevancia(LIST *l);
 #endif
