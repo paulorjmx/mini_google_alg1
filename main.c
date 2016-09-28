740773fbd323497e9f61d671115ed574527acca7
Paulo Ricardo <pauloricardojm@hotmail.com.br>
2016-09-28 16:40:55 -0300

    The difinitive main file

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "inc/interface.h"
 
 int main(int argc, char *argv[])
 {
+    LIST *l = NULL;
+    l = get_from_file("googlebot.txt");
+    menu(l);
+    erase_list(l);
     return 0;
 }
