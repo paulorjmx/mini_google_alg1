eb5ac1cc5b6c37013f17e9bddfbbb4053d3240e0
Paulo Ricardo <pauloricardojm@hotmail.com.br>
2016-09-28 15:29:50 -0300

    Removed to work with Keywords structure

--- inc/list.h
+++ inc/list.h
@@ -1,10 +1,9 @@
 #ifndef LIST_H
     #define LIST_H
-        #include "keywords.h"
         typedef struct list LIST;
 
         LIST *create_list();
-        void insert_site(LIST *l, unsigned int codigo, char nome[], unsigned int relevancia, char link[], KEYWORDS *palavrachave);
+        void insert_site(LIST *l, unsigned int codigo, char nome[], unsigned int relevancia, char link[], char *palavras_chave);
         void update_relevance(LIST *l, unsigned int relevancia, unsigned int cod);
         void remove_site(LIST *l, unsigned cod);
         void erase_list(LIST *l);
