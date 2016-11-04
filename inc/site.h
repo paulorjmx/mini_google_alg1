80923e6c294a42ae6b29dedf32cc60b6abd26a88
paulorjmx <pauloricardojm@hotmail.com.br>
2016-11-04 16:05:10 -0200

    Added necessary headers files and add some functions

--- inc/site.h
+++ inc/site.h
@@ -3,5 +3,8 @@
 
         typedef struct site SITE;
 
-        
+        SITE *site_create(unsigned int code, char *nome, unsigned int relevance, char *link);
+        unsigned int site_get_code(SITE *s);
+        void site_free(SITE **s);
+        void site_to_string(SITE *s);
 #endif
