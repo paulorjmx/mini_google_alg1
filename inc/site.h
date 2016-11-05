1a7fc50ab2829ba562f5646084139fc04e7ddb13
paulo ricardo <pauloricardojm@hotmail.com.br>
2016-11-05 21:32:22 -0200

    Fixed signature of get_keywords function

--- inc/site.h
+++ inc/site.h
@@ -5,10 +5,7 @@
         typedef struct site SITE;
 
         SITE *site_create(unsigned int code, char *nome, unsigned int relevance, char *link, KEYWORDS *k, unsigned int num_keywords);
-<<<<<<< HEAD
         KEYWORDS *site_get_keywords(SITE *s);
-=======
->>>>>>> d5d8b680b28b95bf663ac74718638c0467e3ae96
         unsigned int site_get_code(SITE *s);
         unsigned int site_get_nkeywords(SITE *s);
         void site_free(SITE **s);
