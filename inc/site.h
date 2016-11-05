6b97d21b1379bbbc47c25fdbb88059160e040687
paulo ricardo <pauloricardojm@hotmail.com.br>
2016-11-05 00:42:52 -0200

    functions handle also KEYWORDS data structure

--- inc/site.h
+++ inc/site.h
@@ -1,9 +1,10 @@
 #ifndef SITE_H
     #define SITE_H
+        #include "avltree_keywords.h"
 
         typedef struct site SITE;
 
-        SITE *site_create(unsigned int code, char *nome, unsigned int relevance, char *link, unsigned int num_keywords);
+        SITE *site_create(unsigned int code, char *nome, unsigned int relevance, char *link, KEYWORDS *k, unsigned int num_keywords);
         unsigned int site_get_code(SITE *s);
         unsigned int site_get_nkeywords(SITE *s);
         void site_free(SITE **s);
