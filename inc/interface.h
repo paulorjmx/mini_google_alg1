b4d7b9204d387e1c684107c280ab2a2a15782e41
paulorjmx <pauloricardojm@hotmail.com.br>
2016-11-06 21:37:22 -0200

    Added function to search for a keyword

--- inc/interface.h
+++ inc/interface.h
@@ -9,5 +9,6 @@
         void menu_remover(AVL_SITE *root);
         void menu_inserir_pchave(AVL_SITE *root);
         void menu_atualizar_relevancia(AVL_SITE *root);
+        void menu_busca_palavra(AVL_SITE *root);
         AVL_SITE *get_from_file(char *file_name);
 #endif
