6443ab8b24e00443cae064c40544a88858abe666
paulorjmx <pauloricardojm@hotmail.com.br>
2016-11-06 01:29:00 -0200

    Added function signature to save data into file

--- inc/avltree_keywords.h
+++ inc/avltree_keywords.h
@@ -1,6 +1,6 @@
 #ifndef KEYWORDS_H
     #define KEYWORDS_H
-
+        #include <stdio.h>
         typedef struct avl_keywords KEYWORDS;
 
         KEYWORDS *avlkeywords_create(char *keyword);
@@ -10,6 +10,7 @@
         char *avlkeywords_search_smaller_right(KEYWORDS *root);
         void avlkeywords_free(KEYWORDS **root);
         void avlkeywords_inorder(KEYWORDS *root);
+        void avlkeywords_inorder_file(KEYWORDS *root, FILE *arq);
         int avlkeywords_height(KEYWORDS *root);
         void avlkeywords_rotate_right(KEYWORDS **root);
         void avlkeywords_rotate_left(KEYWORDS **root);
