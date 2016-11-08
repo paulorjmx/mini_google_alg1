9e835e16c8681d0beb4eeb54cf0ad8f88c478dd8
Paulo Ricardo <pauloricardojm@hotmail.com.br>
2016-11-08 16:10:50 -0200

    Added function to search a keyword

--- inc/site.h
+++ inc/site.h
@@ -9,6 +9,8 @@
         KEYWORDS **site_get_keywords(SITE *s);
         unsigned int site_get_code(SITE *s);
         unsigned int site_get_nkeywords(SITE *s);
+        unsigned int site_get_relevance(SITE *s);
+        int site_search_keyword(SITE *s, char *keyword);
         void site_update_nkeywords(SITE *s, unsigned int num_keywords);
         void site_update_relevance(SITE *s, unsigned int relevance);
         void site_free(SITE **s);
